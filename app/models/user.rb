230b7469b968e71af549563308326aa96119a6e3
Bram Plessers <webdevotion@gmail.com>
2018-07-13 12:54:05 +0200

    Not needed here. Works fine as is.
    ```
    $ bundle exec rails c development
       u = User.duplicated_associations
       => [:company]
    ```

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,4 @@ class User < ApplicationRecord
   include Friendable
 
   DUPLICATED_ASSOCATIONS = [:company]
-  def self.duplicated_associations
-    if defined?(super)
-      DUPLICATED_ASSOCATIONS + super
-    else
-      DUPLICATED_ASSOCATIONS
-    end
-  end
-
 end
